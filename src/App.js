80687d1075d4ce47c37c84274228bdb0984ead05
Larhzal Othmane <othmane.larhzal@gmail.com>
2021-08-19 22:56:30 +0100

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
+
 function App() {
+
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: '8ea702a4cd5050e9b05c4fb937992dd7',
+    lat: '-7.362802',
+    lon: '33.702477',
+    lang: 'en',
+    unit: 'metric'
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +28,16 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <ReactWeather
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        data={data}
+        lang="en"
+        locationLabel="Mohammedia"
+        unitLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+        showForecast
+      />
     </div>
   );
 }
